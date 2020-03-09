c65763823f02058c0d4128dc08d0c0f91d63a6c8
Syed Khalil Ullah <khalil.ullah@automicgroup.com.au>
2020-03-09 13:25:40 +1100

    Added data binding and setup the connection between units and amount. Also, setup livedata for spread and button state

--- app/src/main/java/au/cmcmarkets/ticker/feature/orderticket/OrderTicketFragment.kt
+++ app/src/main/java/au/cmcmarkets/ticker/feature/orderticket/OrderTicketFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import au.cmcmarkets.ticker.R
 import au.cmcmarkets.ticker.core.di.viewmodel.ViewModelFactory
 import au.cmcmarkets.ticker.data.model.PriceUpdate
+import au.cmcmarkets.ticker.databinding.FragmentOrderTicketBinding
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_order_ticket.*
 import javax.inject.Inject
 
 class OrderTicketFragment : DaggerFragment() {
@@ -18,36 +20,64 @@ class OrderTicketFragment : DaggerFragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
-    var txtSellValue: TextView? = null
-    var txtBuyValue: TextView? = null
-
     private val viewModel: OrderTicketViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_order_ticket, container, false)
-        txtSellValue = view.findViewById(R.id.txtSellValue)
-        txtBuyValue = view.findViewById(R.id.txtBuyValue)
-        return view
+        val binding: FragmentOrderTicketBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_order_ticket, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val priceObserver = Observer<PriceUpdate> { price ->
+            price?.let {
+                setSellPrice(price.sell)
+                setBuyingPrice(price.buy)
+            }
+        }
+        viewModel.sellingPrice.observe(viewLifecycleOwner, priceObserver)
+
+        val amountObserver = Observer<String> {
+            if (editTxtAmount.hasFocus()) { viewModel.onAmountEnteredByUser() }
+        }
+        viewModel.amountValue.observe(viewLifecycleOwner, amountObserver)
+
+        val unitObserver = Observer<String> {
+            if (editTxtUnits.hasFocus()) { viewModel.onUnitEnteredByUser() }
+        }
+        viewModel.unitValue.observe(viewLifecycleOwner, unitObserver)
+
+    }
+
+    private fun setSellPrice(sellingPrice: Double) {
+        val components = sellingPrice.toString().split(".")
+        if (components.size > 1) {
+            txtSellValueWhole.text = components[0]
+            txtSellValueFraction.text = String.format(".%s", components[1])
+        }
+    }
+
+    private fun setBuyingPrice(buyingPrice: Double) {
+        val components = buyingPrice.toString().split(".")
+        if (components.size > 1) {
+            txtBuyValueWhole.text = components[0]
+            txtBuyValueFraction.text = String.format(".%s", components[1])
+        }
     }
 
     override fun onPause() {
         super.onPause()
+        viewModel.stopPolling()
     }
 
     override fun onResume() {
         super.onResume()
-        val priceObserver = Observer<PriceUpdate> { price ->
-            price?.let {
-                txtSellValue?.text = price.sell.toString()
-                txtBuyValue?.text = price.buy.toString()
-            }
-
-        }
-        viewModel.sellingPrice.observe(this, priceObserver)
-        viewModel.fetchPrices()
+        viewModel.startPolling()
     }
 }
--- app/src/main/java/au/cmcmarkets/ticker/feature/orderticket/OrderTicketViewModel.kt
+++ app/src/main/java/au/cmcmarkets/ticker/feature/orderticket/OrderTicketViewModel.kt
@@ -1,6 +1,9 @@
 package au.cmcmarkets.ticker.feature.orderticket
 
+import android.os.Handler
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import au.cmcmarkets.ticker.data.api.BitcoinApi
 import au.cmcmarkets.ticker.data.model.PriceUpdate
@@ -8,16 +11,80 @@ import au.cmcmarkets.ticker.data.model.Ticker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 import javax.inject.Inject
+import kotlin.concurrent.timerTask
 
 
 class OrderTicketViewModel @Inject constructor(private val bitcoinApi: BitcoinApi) : ViewModel() {
+
+    companion object {
+        const val POLLING_INTERVAL_MILLIS: Long = 15000
+    }
 
     val sellingPrice: MutableLiveData<PriceUpdate> by lazy {
         MutableLiveData<PriceUpdate>()
     }
 
-    fun fetchPrices() {
+    val highlightValues: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    val amountValue: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val unitValue: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val enableConfirmButton: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    val spreadValue: LiveData<String> = Transformations.map(sellingPrice){ (it.buy - it.sell).toString() }
+
+    fun startPolling() {
+        pollingTimer.schedule(timerTask { fetchPrices() }, 0, POLLING_INTERVAL_MILLIS)
+    }
+
+    fun stopPolling() {
+        pollingTimer.cancel()
+    }
+
+    fun onAmountEnteredByUser() {
+        amountValue.value?.let {
+            val buy = sellingPrice.value?.let { it.buy } ?: return
+            val amount = it.toIntOrNull()
+
+            val units = amount?.let { (amount/buy).toString() } ?: ""
+            unitValue.value = units
+            enableConfirmButton.value = it.isValid() && units.isValid()
+        }
+    }
+
+    fun onUnitEnteredByUser() {
+        unitValue.value?.let {
+            val buy = sellingPrice.value?.let { it.buy } ?: return
+            val units = it.toIntOrNull()
+            val amount = units?.let { (units*buy).toString() } ?: ""
+            amountValue.value = amount
+            enableConfirmButton.value = it.isValid() && amount.isValid()
+        }
+    }
+
+    private fun String?.isValid(): Boolean {
+        if (this != null) {
+            return this != "0" && this.isNotEmpty()
+        }
+        return false
+    }
+
+    private val pollingTimer by lazy {
+        Timer("PricePollTimer", false)
+    }
+
+    private fun fetchPrices() {
         bitcoinApi.fetchPrices()?.enqueue(object : Callback<Ticker> {
 
             override fun onFailure(call: Call<Ticker>?, t: Throwable?) {
@@ -25,11 +92,15 @@ class OrderTicketViewModel @Inject constructor(private val bitcoinApi: BitcoinAp
             }
 
             override fun onResponse(call: Call<Ticker>?, response: Response<Ticker>?) {
-                if (response?.isSuccessful!!) {
-                    val ticker = response.body()
-                    sellingPrice.value = ticker?.priceUpdate ?: return
-                } else {
-                    print(response?.errorBody()?.string())
+                response?.let {
+                    if (response.isSuccessful) {
+                        val ticker = response.body()
+                        sellingPrice.value = ticker?.priceUpdate ?: return
+                        highlightValues.value = true
+                        Handler().postDelayed({ highlightValues.value = false }, 2000)
+                    } else {
+                        print(response.errorBody()?.string())
+                    }
                 }
             }
 
